e31ac9885b5961b5bc4e27a4817d60aa62e148f5
Ben <benedict_sun@dlsu.edu.ph>
2021-08-14 21:12:43 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s17/sun/benedict/teamwork/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.s17.sun.benedict.teamwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
