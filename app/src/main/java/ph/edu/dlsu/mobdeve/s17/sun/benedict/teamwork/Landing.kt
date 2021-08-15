516547828e6a5094335c531f78410d4c18e4a74b
Ben <benedict_sun@dlsu.edu.ph>
2021-08-15 17:58:47 +0800

    Progress on login/register

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s17/sun/benedict/teamwork/Landing.kt
@@ -0,0 +1,43 @@
+package ph.edu.dlsu.mobdeve.s17.sun.benedict.teamwork
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import ph.edu.dlsu.mobdeve.s17.sun.benedict.teamwork.databinding.FragmentLandingBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LandingFragment : Fragment() {
+
+    lateinit var binding: FragmentLandingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLandingBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Initialize event handlers
+        setEventHandlers(view)
+
+        // Return the entire fragment
+        return view
+    }
+
+    private fun setEventHandlers(view : View){
+        binding.btnLogin.setOnClickListener {
+            view.findNavController().navigate(R.id.navigateToLogin)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            view.findNavController().navigate(R.id.navigateToRegister)
+        }
+    }
+}
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s17/sun/benedict/teamwork/Login.kt
@@ -0,0 +1,30 @@
+package ph.edu.dlsu.mobdeve.s17.sun.benedict.teamwork
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ph.edu.dlsu.mobdeve.s17.sun.benedict.teamwork.databinding.FragmentLoginBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    lateinit var binding : FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
+    }
+
+}
